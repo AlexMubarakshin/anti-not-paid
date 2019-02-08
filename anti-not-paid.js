31322d279cfb5a914e06a92e2d5750f9f5c2ad10
AlexMubarakshin <muban96@mail.ru>
2019-02-08 14:43:13 +0300

    Fix stupid mistake

--- anti-not-paid.js
+++ anti-not-paid.js
@@ -1,6 +1,7 @@
 (
     function () {
-        if (!!document.getElementsByTagName("BODY")[0].style.opacity) {
+        if (!document.getElementsByTagName("BODY")[0].style.opacity ||
+            parseFloat(document.getElementsByTagName("BODY")[0].style.opacity) < 1) {
             document.getElementsByTagName("BODY")[0].style.opacity = 1;
         }
     }
