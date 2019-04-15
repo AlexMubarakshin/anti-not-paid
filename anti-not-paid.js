dcac33de3105d4b9cd6572ef97fa9c42a9d45891
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2019-04-15 01:16:35 -0400

    Made it a one-liner

--- anti-not-paid.js
+++ anti-not-paid.js
@@ -1,8 +1,1 @@
-(
-    function () {
-        if (!document.getElementsByTagName("BODY")[0].style.opacity ||
-            parseFloat(document.getElementsByTagName("BODY")[0].style.opacity) < 1) {
-            document.getElementsByTagName("BODY")[0].style.opacity = 1;
-        }
-    }
-)()
+(() => {document.body.style.opacity = 1;})()
